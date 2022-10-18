261752451853f701abcfaf2bd7d88f0b4bb73f66
Giuseppe Laudani <26256050+giuseppeLau@users.noreply.github.com>
2022-10-18 10:51:59 +0200

    Update middle-square-method.cpp

--- middle-square-method.cpp
+++ middle-square-method.cpp
@@ -3,22 +3,22 @@
 
 using namespace std;
 
-string middleSquareMethod(int seed){
+string middleSquareMethod(int seed) {
 
-	string str = to_string(seed);				//Converts seed from int to string
-	int seedLength = str.length();				//Stores in a variable the number of digits
+	string str = to_string(seed);			//Converts seed from int to string
+	int seedLength = str.length();			//Stores in a variable the number of digits
 	
-	if ((seedLength%2)!=0) {					//Correction in case the seed has an odd number of digits
-        str.insert(0, "0"); 					//Adding a 0 at the beginning of the string 
-        seedLength += 1;						//Increse of lenght variable
-    }
+	if ((seedLength%2)!=0) {			//Correction in case the seed has an odd number of digits
+        	str.insert(0, "0"); 			//Adding a 0 at the beginning of the string 
+        	seedLength += 1;			//Increse of lenght variable
+    	}
 
-    string sqrStr = to_string(seed*seed);		//Conversion of the square of the root from int to string
+	string sqrStr = to_string(seed*seed);		//Conversion of the square of the root from int to string
 
-    while(sqrStr.length() < seedLength*2)		//Checks that the number of digits of the square of the seed is at least 2*seedLenght
-    		sqrStr.insert(0, "0");				//(algorithm requirement) otherwise a padding of zeros is added									
+    	while(sqrStr.length() < seedLength*2)		//Checks that the number of digits of the square of the seed is at least 2*seedLenght
+		sqrStr.insert(0, "0");			//(algorithm requirement) otherwise a padding of zeros is added									
 
-	string result = "";							//Result Generation
+	string result = "";				//Result Generation
 
 	for (int i = (sqrStr.length()/2)/2; i < sqrStr.length()/2; i++) 
 		result += sqrStr.at(i);
